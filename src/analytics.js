7628cae8a3a6659b2110b4d9b321a07b555ed872
Yuriy Husnay <husnaj23@gmail.com>
2016-09-01 19:25:19 +0300

    analytics fix

--- src/analytics.js
+++ src/analytics.js
@@ -2,15 +2,8 @@
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://ssl.google-analytics.com/analytics.js','ga');
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-51673902-1', 'auto');
-ga('set', 'checkProtocolTask', function(){ /* nothing */ });
-var version;
-try {
-  version = chrome.runtime.getManifest().version
-} catch (e) {
-  version = 'error';
-}
-ga('set', 'appVersion', version);
-ga('send', 'pageview');
+ga('set', 'checkProtocolTask', null);
+ga('send', 'pageview', '/index.html');
