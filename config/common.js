990ad11ed41441ed83ec8e7f6f70370f0c2a3cff
Yuriy Husnay <husnaj23@gmail.com>
2016-09-06 15:06:52 +0300

    more usual configuration naming

--- config/common.js
+++ config/common.js
@@ -4,27 +4,33 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   loaders: {
+
     eslint: {
       test: /\.js$/,
       loader: 'eslint',
       include: './src'
     },
+
     babel: {
       test: /\.js$/,
       exclude: /node_modules/,
       loaders: ['babel']
     },
-    stylus_dev: {
-      test: /\.styl/,
-      loader: 'style!css!stylus'
+
+    stylus: {
+      develop: {
+        test: /\.styl/,
+        loader: 'style!css!stylus'
+      },
+      production: {
+        test: /\.styl/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: 'css-loader!stylus-loader'
+        })
+      }
     },
-    stylus_dist: {
-      test: /\.styl/,
-      loader: ExtractTextPlugin.extract({
-        fallbackLoader: 'style-loader',
-        loader: 'css-loader!stylus-loader'
-      })
-    },
+
     fonts: {
       test: /\.woff/,
       loader: 'url',
@@ -34,37 +40,43 @@ module.exports = {
       }
     }
   },
+
   plugins: {
+
     options: new webpack.LoaderOptionsPlugin({
       minimize: true,
       debug: false
     }),
+
     html: {
-      dev: new HtmlWebpackPlugin({
+      develop: new HtmlWebpackPlugin({
         template: './src/index.html',
         title: 'New Tab',
         cache: true,
         ENV: 'dev'
       }),
-      dist: new HtmlWebpackPlugin({
+      production: new HtmlWebpackPlugin({
         template: './src/index.html',
         title: 'New Tab',
         hash: true,
         ENV: 'dist'
       })
     },
+
     css: new ExtractTextPlugin('[name]_[hash].css'),
+
     define: {
-      dev: new webpack.DefinePlugin({
-        'ENV': JSON.stringify('dev')
+      develop: new webpack.DefinePlugin({
+        'ENV': JSON.stringify('develop')
       }),
-      dist: new webpack.DefinePlugin({
-        'ENV': JSON.stringify('dist'),
+      production: new webpack.DefinePlugin({
+        'ENV': JSON.stringify('production'),
         'process.env':{
           'NODE_ENV': JSON.stringify('production')
         }
       })
     },
+
     uglify: new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
