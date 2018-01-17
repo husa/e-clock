edeb27b764c10d3170719932f25a22639b19a88a
Yuriy Husnay <husnaj23@gmail.com>
2018-01-17 15:56:32 +0200

    tweak am\pm alignment

--- src/services/location.js
+++ src/services/location.js
@@ -1,13 +1,27 @@
-const positionOptions = {
+// @flow
+
+type PositionOptions = {
+  enableHighAccuracy: boolean,
+  timeout: number,
+  maximumAge: number
+};
+
+export type PositionErrorObject = {
+  type: 'PositionError',
+  message: string,
+  error: PositionError
+};
+
+const positionOptions: PositionOptions = {
   enableHighAccuracy: true,
-  timeout: 0, //5 * 1000, // 5 seconds
+  timeout: 5 * 1000, // 5 seconds
   maximumAge: 60 * 60 * 1000 // 1 hour
 };
 
 const TIMEOUT_INCREASE = 1000;
 
 class Location {
-  getPosition (options = positionOptions) {
+  getPosition (options?: PositionOptions = positionOptions): Promise<*> {
     return new Promise((resolve, reject) => {
       navigator.geolocation.getCurrentPosition(resolve, reject, options);
     }).then(
