6b640d5c566c3b62d0aeccb7fee462728d7615f7
husa <1536549+husa@users.noreply.github.com>
2024-06-17 11:50:18 +0300

    remove old "share" links

--- src/components/settings/AboutSettings.jsx
+++ src/components/settings/AboutSettings.jsx
@@ -25,44 +25,44 @@ class AboutSettings extends Component {
             </svg>
             <span>{lang.t('Rate')}</span>
           </a>
-          <div className='settings-about__share'>
-            <a
-              className='settings-about__share-link settings-about__share-link--twitter'
-              href='https://twitter.com/intent/tweet/?text=Checkout%20e-clock%20-%20minimalistic%20chrome%20extension&url=https%3A%2F%2Fchrome.google.com%2Fwebstore%2Fdetail%2Fe-clock%2Fojhhcfhabhligodffabdhcaoicecaboo&via=YuriyHusnay&hashtags=e-clock,chrome,extension'
-              target='_blank'
-              rel='noopener noreferrer'>
-              <svg width='24px' height='24px' viewBox='0 0 24 24'>
-                <use xlinkHref='#social-twitter'></use>
-              </svg>
-            </a>
-            <a
-              className='settings-about__share-link settings-about__share-link--facebook'
-              href='https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fchrome.google.com%2Fwebstore%2Fdetail%2Fe-clock%2Fojhhcfhabhligodffabdhcaoicecaboo'
-              target='_blank'
-              rel='noopener noreferrer'>
-              <svg width='24px' height='24px' viewBox='0 0 24 24'>
-                <use xlinkHref='#social-facebook'></use>
-              </svg>
-            </a>
-            <a
-              className='settings-about__share-link settings-about__share-link--google'
-              href='https://plus.google.com/share?url=https%3A%2F%2Fchrome.google.com%2Fwebstore%2Fdetail%2Fe-clock%2Fojhhcfhabhligodffabdhcaoicecaboo'
-              target='_blank'
-              rel='noopener noreferrer'>
-              <svg width='24px' height='24px' viewBox='0 0 24 24'>
-                <use xlinkHref='#social-google'></use>
-              </svg>
-            </a>
-            <a
-              className='settings-about__share-link settings-about__share-link--reddit'
-              href='http://www.reddit.com/submit/?url=https%3A%2F%2Fchrome.google.com%2Fwebstore%2Fdetail%2Fe-clock%2Fojhhcfhabhligodffabdhcaoicecaboo'
-              target='_blank'
-              rel='noopener noreferrer'>
-              <svg width='24px' height='24px' viewBox='0 0 24 24'>
-                <use xlinkHref='#social-reddit'></use>
-              </svg>
-            </a>
-          </div>
+          {/* <div className='settings-about__share'> */}
+          {/*   <a */}
+          {/*     className='settings-about__share-link settings-about__share-link--twitter' */}
+          {/*     href='https://twitter.com/intent/tweet/?text=Checkout%20e-clock%20-%20minimalistic%20chrome%20extension&url=https%3A%2F%2Fchrome.google.com%2Fwebstore%2Fdetail%2Fe-clock%2Fojhhcfhabhligodffabdhcaoicecaboo&via=YuriyHusnay&hashtags=e-clock,chrome,extension' */}
+          {/*     target='_blank' */}
+          {/*     rel='noopener noreferrer'> */}
+          {/*     <svg width='24px' height='24px' viewBox='0 0 24 24'> */}
+          {/*       <use xlinkHref='#social-twitter'></use> */}
+          {/*     </svg> */}
+          {/*   </a> */}
+          {/*   <a */}
+          {/*     className='settings-about__share-link settings-about__share-link--facebook' */}
+          {/*     href='https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fchrome.google.com%2Fwebstore%2Fdetail%2Fe-clock%2Fojhhcfhabhligodffabdhcaoicecaboo' */}
+          {/*     target='_blank' */}
+          {/*     rel='noopener noreferrer'> */}
+          {/*     <svg width='24px' height='24px' viewBox='0 0 24 24'> */}
+          {/*       <use xlinkHref='#social-facebook'></use> */}
+          {/*     </svg> */}
+          {/*   </a> */}
+          {/*   <a */}
+          {/*     className='settings-about__share-link settings-about__share-link--google' */}
+          {/*     href='https://plus.google.com/share?url=https%3A%2F%2Fchrome.google.com%2Fwebstore%2Fdetail%2Fe-clock%2Fojhhcfhabhligodffabdhcaoicecaboo' */}
+          {/*     target='_blank' */}
+          {/*     rel='noopener noreferrer'> */}
+          {/*     <svg width='24px' height='24px' viewBox='0 0 24 24'> */}
+          {/*       <use xlinkHref='#social-google'></use> */}
+          {/*     </svg> */}
+          {/*   </a> */}
+          {/*   <a */}
+          {/*     className='settings-about__share-link settings-about__share-link--reddit' */}
+          {/*     href='http://www.reddit.com/submit/?url=https%3A%2F%2Fchrome.google.com%2Fwebstore%2Fdetail%2Fe-clock%2Fojhhcfhabhligodffabdhcaoicecaboo' */}
+          {/*     target='_blank' */}
+          {/*     rel='noopener noreferrer'> */}
+          {/*     <svg width='24px' height='24px' viewBox='0 0 24 24'> */}
+          {/*       <use xlinkHref='#social-reddit'></use> */}
+          {/*     </svg> */}
+          {/*   </a> */}
+          {/* </div> */}
         </div>
         <div className='settings-about__author-info'>
           <p>
