607983029337a4a2cea2f1116163843a088b9250
husa <1536549+husa@users.noreply.github.com>
2024-06-17 11:50:31 +0300

    switch to tsx

--- src/components/error/ErrorMessage.tsx
+++ src/components/error/ErrorMessage.tsx
@@ -3,7 +3,11 @@ import './errorMessage.scss';
 import React from 'react';
 import classNames from 'classnames';
 
-const ErrorMessage = ({ className, children }) => (
+type Props = React.PropsWithChildren<{
+  className: string;
+}>;
+
+const ErrorMessage = ({ className, children }: Props) => (
   <div className={classNames('error', className)}>
     <svg className='error__icon' height='24' viewBox='0 0 24 24' width='24'>
       <use xlinkHref='#error'></use>
