30517a1632ff70cc30f58cb2cb8ab31288437856
husa <1536549+husa@users.noreply.github.com>
2024-08-02 14:38:04 +0300

    cleanup

--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -1,28 +1,3 @@
-// import { createStore, combineReducers, applyMiddleware, Middleware } from 'redux';
-// import { createLogger } from 'redux-logger';
-// import { thunk } from 'redux-thunk';
-//
-// import reducers from '../reducers';
-// import middleware from './middleware';
-//
-// const middlewares = [thunk, ...middleware];
-//
-// const logger = createLogger({
-//   duration: true,
-//   collapsed: true,
-// });
-//
-// if (ENV === 'development') middlewares.push(logger);
-//
-// const createStoreWithMiddleware = applyMiddleware(...(middlewares as Middleware[]))(createStore);
-//
-// const reducer = combineReducers({
-//   ...reducers,
-// });
-//
-// const createAppStore = (initialState = {}) => createStoreWithMiddleware(reducer, initialState);
-//
-// export default createAppStore;
 import { configureStore } from '@reduxjs/toolkit';
 import settingsReducer, { SettingsState } from './slices/settingsSlice';
 import dockReducer, { DockState } from './slices/dockSlice';
@@ -45,10 +20,3 @@ const createAppStore = (initialState: Partial<AppState> = {}) =>
   });
 
 export default createAppStore;
-
-// Infer the `RootState` and `AppDispatch` types from the store itself
-// export type RootState = ReturnType<typeof store.getState>
-// type B = ReturnType<typeof createAppStore>;V
-// type A = ReturnType<B>;
-// export type RootState = ReturnType<A['getState']>;
-// export type AppDispatch = typeof store.dispatch;
--- src/store/middleware/chromeStorage.ts
+++ src/store/middleware/chromeStorage.ts
@@ -1,10 +1,5 @@
-import { debounce } from '../../utils';
-
-// import { SET_OPTIONS } from '../../actions/settings';
-// import { SET_DOCK_OPTIONS } from '../../actions/dock';
-// import { SET_INTRO } from '../../actions/intro';
-
 import { createListenerMiddleware, isAnyOf } from '@reduxjs/toolkit';
+import { debounce } from '../../utils';
 import storage from '../../services/storage';
 import { AppState } from '../createStore';
 import { setOptions as setDockOptions } from '../slices/dockSlice';
@@ -12,29 +7,10 @@ import { setOptions as setSettingsOptions } from '../slices/settingsSlice';
 
 const deboucedStorageSync = debounce((state: AppState) => storage.sync(state), 1000);
 
-// const actions = [
-//   // SET_INTRO, // after welcome screen
-//   // SET_DOCK_OPTIONS, // when any dock options were changed
-//   // SET_OPTIONS, // when any other options were changed
-//   setDockOptions.type, // when any dock options were changed
-//   setSettingsOptions.type, // when any other options were changed
-// ];
-//
-// const chromeStorage =
-//   ({ getState }) =>
-//   (next) =>
-//   (action) => {
-//     const result = next(action);
-//     if (!actions.includes(action.type)) return result;
-//     deboucedStorageSync(getState());
-//     return result;
-//   };
-//
-// export default chromeStorage;
-
-// new approach
+// create instance of listener middleware
 export const syncSettingsToChromeStorageMiddleware = createListenerMiddleware<AppState>();
 
+// "start" listening for actions
 syncSettingsToChromeStorageMiddleware.startListening({
   predicate: isAnyOf(setDockOptions, setSettingsOptions),
   effect(_action, listenerApi) {
