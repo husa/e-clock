2c264253e25475957e0053f1a6afef8c264ea4c6
husa <1536549+husa@users.noreply.github.com>
2024-08-13 00:24:23 +0300

    build: upgrade dependencies, migrate storybook to rsbuild
    "storybook-react-rsbuild" package has to be locked to 0.0.11 atm
    Current version 0.0.12 doesn't work(apparently without publicPath or
    smth in rsbuild config) and I don't care that much right now

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginSass } from '@rsbuild/plugin-sass';
 import { pluginTypeCheck } from '@rsbuild/plugin-type-check';
 
 export default defineConfig({
-  plugins: [pluginSass(), pluginTypeCheck()],
+  plugins: [pluginReact(), pluginSass(), pluginTypeCheck()],
   tools: {
     cssLoader: {
       url: false,
