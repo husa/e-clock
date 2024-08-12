666de9dfcd70f7bbaf96a105b47b19b351df7768
husa <1536549+husa@users.noreply.github.com>
2024-08-13 00:43:12 +0300

    fix: typescript errors for orphaned files

--- src/store/middleware/weather.ts
+++ src/store/middleware/weather.ts
@@ -1,28 +1,28 @@
-import { selectWeatherData, selectSettings, selectLocationName } from '../../__selectors';
-
-import { loadWeather } from '../../__actions/weather';
-
-const weatherBackgroundService =
-  ({ getState, dispatch }) =>
-  (next) =>
-  (action) => {
-    const result = next(action);
-    const state = getState();
-    // TODO: check for particular action types
-    // if displayWeather is true and there's no weather data for current location
-    // fetch weather for current location
-    const weather = selectWeatherData(state);
-
-    const { displayWeather } = selectSettings(state);
-    if (!displayWeather) return result;
-    if (weather || (weather && weather.loading) || (weather && weather.error)) return result;
-
-    // if (displayWeather && (!weather || !weather.data) && !weather.loading && !weather.error) {
-    const locationName = selectLocationName(state);
-    // schedule load weather action dispatching on next loop
-    Promise.resolve().then(() => dispatch(loadWeather(locationName)));
-    // }
-    return result;
-  };
-
-export default weatherBackgroundService;
+// import { selectWeatherData, selectSettings, selectLocationName } from '../../__selectors';
+//
+// import { loadWeather } from '../../__actions/weather';
+//
+// const weatherBackgroundService =
+//   ({ getState, dispatch }) =>
+//   (next) =>
+//   (action) => {
+//     const result = next(action);
+//     const state = getState();
+//     // TODO: check for particular action types
+//     // if displayWeather is true and there's no weather data for current location
+//     // fetch weather for current location
+//     const weather = selectWeatherData(state);
+//
+//     const { displayWeather } = selectSettings(state);
+//     if (!displayWeather) return result;
+//     if (weather || (weather && weather.loading) || (weather && weather.error)) return result;
+//
+//     // if (displayWeather && (!weather || !weather.data) && !weather.loading && !weather.error) {
+//     const locationName = selectLocationName(state);
+//     // schedule load weather action dispatching on next loop
+//     Promise.resolve().then(() => dispatch(loadWeather(locationName)));
+//     // }
+//     return result;
+//   };
+//
+// export default weatherBackgroundService;
