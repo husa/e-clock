6ffffd43b97bcb162b7895761fe28bbac2da42bb
husa <1536549+husa@users.noreply.github.com>
2025-01-22 11:30:21 +0100

    feat: add firefox support

--- src/components/Settings/SettingsPanelAbout/SettingsPanelAbout.tsx
+++ src/components/Settings/SettingsPanelAbout/SettingsPanelAbout.tsx
@@ -1,7 +1,9 @@
 import './SettingsPanelAbout.scss';
 import { SettingsPanel } from '../SettingsPanel/SettingsPanel';
 import lang from '../../../services/lang';
+import { currentRuntime } from '../../../utils/currentRuntime';
 
+// TODO: add link to Mozilla Add-ons
 export const SettingsPanelAbout = () => {
   const manifest = chrome.runtime.getManifest();
 
@@ -9,14 +11,16 @@ export const SettingsPanelAbout = () => {
     <SettingsPanel className="settings-panel-about">
       <h1>{manifest.name}</h1>
       <h3>{manifest.version}</h3>
-      <a
-        href={`https://chrome.google.com/webstore/detail/${chrome.runtime.id}`}
-        className="settings-about__like">
-        <svg viewBox="0 0 24 24" width="24" height="24">
-          <use xlinkHref="#favorite"></use>
-        </svg>
-        <span>{lang.t('i18nRate')}</span>
-      </a>
+      {currentRuntime === 'chrome' && (
+        <a
+          href={`https://chrome.google.com/webstore/detail/${chrome.runtime.id}`}
+          className="settings-about__like">
+          <svg viewBox="0 0 24 24" width="24" height="24">
+            <use xlinkHref="#favorite"></use>
+          </svg>
+          <span>{lang.t('i18nRate')}</span>
+        </a>
+      )}
     </SettingsPanel>
   );
 };
