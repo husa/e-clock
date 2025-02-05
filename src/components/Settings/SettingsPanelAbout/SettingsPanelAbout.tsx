84f9023b573b46a293b4d711ec6007238fb1fdfc
husa <1536549+husa@users.noreply.github.com>
2025-02-05 14:15:16 +0200

    fix: add firefox "rate" button

--- src/components/Settings/SettingsPanelAbout/SettingsPanelAbout.tsx
+++ src/components/Settings/SettingsPanelAbout/SettingsPanelAbout.tsx
@@ -3,24 +3,24 @@ import { SettingsPanel } from '../SettingsPanel/SettingsPanel';
 import lang from '../../../services/lang';
 import { currentRuntime } from '../../../utils/currentRuntime';
 
-// TODO: add link to Mozilla Add-ons
 export const SettingsPanelAbout = () => {
   const manifest = chrome.runtime.getManifest();
+  const extentionId = chrome.runtime.id;
+  const storeUrl =
+    currentRuntime === 'chrome'
+      ? `https://chrome.google.com/webstore/detail/${extentionId}`
+      : `https://addons.mozilla.org/firefox/addon/${extentionId}`;
 
   return (
     <SettingsPanel className="settings-panel-about">
       <h1>{manifest.name}</h1>
       <h3>{manifest.version}</h3>
-      {currentRuntime === 'chrome' && (
-        <a
-          href={`https://chrome.google.com/webstore/detail/${chrome.runtime.id}`}
-          className="settings-about__like">
-          <svg viewBox="0 0 24 24" width="24" height="24">
-            <use xlinkHref="#favorite"></use>
-          </svg>
-          <span>{lang.t('i18nRate')}</span>
-        </a>
-      )}
+      <a href={storeUrl} className="settings-about__like">
+        <svg viewBox="0 0 24 24" width="24" height="24">
+          <use xlinkHref="#favorite"></use>
+        </svg>
+        <span>{lang.t('i18nRate')}</span>
+      </a>
     </SettingsPanel>
   );
 };
